22847890bc3d528edd7cf203c51c2b99f1214a03
ecclesias <77536370+zeviel@users.noreply.github.com>
2023-01-06 11:10:00 +0600

    More readability, added annotations

--- src/utils.py
+++ src/utils.py
@@ -6,15 +6,16 @@ from tabulate import tabulate
 from src.library import amino
 from concurrent.futures import ThreadPoolExecutor
 
-
 accounts = []
 with open("accounts.json") as database:
 	data = load(database)
 	for account in data:
 		accounts.append(account)
 
-
-def login(amino: amino.Amino, email: str, password: str):
+def login(
+		amino: amino.Amino,
+		email: str,
+		password: str) -> None:
 	try:
 		print(f"[deviceID]::: {amino.device_id}")
 		amino.login(
@@ -26,7 +27,7 @@ def login(amino: amino.Amino, email: str, password: str):
 		print(f"[Error in login]::: {e}")
         
         
-def get_timers():
+def get_timers() -> dict:
 	return {
 		"start": int(time()),
 		"end": int(time()) + 300
@@ -36,7 +37,7 @@ def coin_generator(
 		amino: amino.Amino,
 		ndc_id: int,
 		email: str,
-		delay: int):
+		delay: int) -> None:
 	timers = [
 		get_timers() for _ in range(50)
 	]
@@ -47,7 +48,7 @@ def generate_coins(
 		amino: amino.Amino,
 		ndc_id: int,
 		email: str,
-		delay: int):
+		delay: int) -> None:
 	Thread(
 		target=coin_generator,
 		args=(
@@ -57,24 +58,23 @@ def generate_coins(
 			delay)
 		).start()
 	
-	
-def play_lottery(amino: amino.Amino, ndc_id: int):
+def play_lottery(
+		amino: amino.Amino,
+		ndc_id: int) -> None:
 	try:
-		lottery = amino.lottery(ndc_id=ndc_id)["api:message"]
-		print(f"[Lottery]::: {lottery}")
+		response = amino.lottery(ndc_id=ndc_id)["api:message"]
+		print(f"[Lottery]::: {response}")
 	except Exception as e:
 		print(f"[Error in play lottery]::: {e}")
 		
-		
-def watch_ad(amino: amino.Amino):
+def watch_ad(amino: amino.Amino) -> None:
 	try:
-		watch_ad = amino.watch_ad()["api:message"]
-		print(f"[Watch ad]::: {watch_ad}")
+		response = amino.watch_ad()["api:message"]
+		print(f"[Watch ad]::: {response}")
 	except Exception as e:
 		print(f"[Error in watch ad]::: {e}")
 		
-		
-def transfer_coins():
+def transfer_coins() -> None:
 	link_info = amino.Amino().get_from_code(
 		input("[Blog link]::: "))["linkInfoV2"]["extensions"]["linkInfo"]
 	ndc_id = link_info["ndcId"]
@@ -85,29 +85,32 @@ def transfer_coins():
 		email = account["email"]
 		password = account["password"]
 		try:
-			login(amino=amino, email=email, password=password)
+			login(
+				amino=amino,
+				email=email,
+				password=password)
 			amino.join_community(ndc_id=ndc_id)
 			total_coins = amino.get_wallet_info()["wallet"]["totalCoins"]
 			print(f"[{email} total coins]::: {total_coins}")
 			if total_coins != 0:
-				transfer = amino.send_coins_blog(
+				response = amino.send_coins_blog(
 					ndc_id=ndc_id,
 					blog_Id=blog_id,
 					coins=total_coins)["api:message"]
-				print(f"[{email} sent]::: {total_coins} coins - {transfer}")
+				print(f"[{email} sent]::: {total_coins} coins - {response}")
 			elif total_coins > 500:
 				total_coins = 500
-				transfer = amino.send_coins_blog(
+				response = amino.send_coins_blog(
 					ndc_id=ndc_id,
 					blog_Id=blog_id,
 					coins=total_coins)["api:message"]
-				print(f"[{email} sent]::: {total_coins} coins - {transfer}")
+				print(f"[{email} sent]::: {total_coins} coins - {response}")
 			sleep(delay)
 		except Exception as e:
 			print(f"[Error in transfer coins]::: {e}")
 
 
-def main_process():
+def main_process() -> None:
 	ndc_id = amino.Amino().get_from_code(
 		input("[Community link]::: "))["linkInfoV2"]["extensions"]["community"]["ndcId"]
 	delay = int(input("[Generation delay in seconds]::: "))
@@ -116,16 +119,15 @@ def main_process():
 		email = account["email"]
 		password = account["password"]
 		try:
-			login(amino=amino, email=email, password=password)
+			login(
+				amino=amino,
+				email=email,
+				password=password)
 			watch_ad(amino=amino)
 			play_lottery(amino=amino, ndc_id=ndc_id)
 			with ThreadPoolExecutor(max_workers=100) as executor: 
-				[executor.submit(
-						generate_coins(
-							amino,
-							ndc_id,
-							email, 
-							delay)) for _ in range(25)]
+				[executor.submit(generate_coins(
+						amino, ndc_id, email, delay)) for _ in range(25)]
 			sleep(delay)
 		except Exception as e:
 			print(f"[Error in main process]::: {e}")
